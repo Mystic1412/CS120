99eb388e4cd1b14c99c7c1e08b4a647fc7aa8a0a
Mystic <nicholelin3929@gmail.com>
2024-08-26 10:38:23 -0400

    making a triangle in C

--- Test.c
+++ Test.c
@@ -2,7 +2,22 @@
 
 int main(){
 
-    printf("Hello CS120!");
-    return 0;
+    printf("Hello CS120!\n");
 
+    for(int x = 0; x<3; x++){
+        for(int y = 0; y<5;y++){
+            if(y+x >= 2 && y-x <=2){
+                printf("*");
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+    //single line comment
+    /*multi
+    line
+    comment*/
 }
