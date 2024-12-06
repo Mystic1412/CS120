074de0679efc9b7038399a94f6caf946a09b1a4e
Nichole L <nicholelin3929@gmail.com>
2024-12-06 15:41:06 -0500

    learned printing in MIPS

--- MIPS Practice/printing.s
+++ MIPS Practice/printing.s
@@ -1,10 +1,19 @@
 .data
-x: .asciiz "Hello"
+x: .asciiz "Hello\n"
 .text
 
 main:
 li $v0, 4 #set syscall code for print string
 la $a0, x #load address of the string
+syscall
+
+
+li $v0, 11 #code for printing character
+la $a0, 78
+syscall
+
+li $v0, 1 #code for printing integer
+la $a0, 3905
 syscall
 
 li $v0, 10 #exits the code
